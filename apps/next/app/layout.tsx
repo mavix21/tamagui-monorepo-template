dadd4d5fc9db8d872774a8faf536995400971559
mavix21 <marcevizcarra2109@hotmail.com>
2025-07-07 22:26:26 -0500

    feat: tooling config

--- apps/next/app/layout.tsx
+++ apps/next/app/layout.tsx
@@ -1,13 +1,17 @@
-import type { Metadata } from 'next'
-import { NextTamaguiProvider } from 'app/provider/NextTamaguiProvider'
+import type { Metadata } from "next";
+import { NextTamaguiProvider } from "app/provider/NextTamaguiProvider";
 
 export const metadata: Metadata = {
-  title: 'Tamagui • App Router',
-  description: 'Tamagui, Solito, Expo & Next.js',
-  icons: '/favicon.ico',
-}
+  title: "Tamagui • App Router",
+  description: "Tamagui, Solito, Expo & Next.js",
+  icons: "/favicon.ico",
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     // You can use `suppressHydrationWarning` to avoid the warning about mismatched content during hydration in dev mode
     <html lang="en" suppressHydrationWarning>
@@ -15,5 +19,5 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <NextTamaguiProvider>{children}</NextTamaguiProvider>
       </body>
     </html>
-  )
+  );
 }
